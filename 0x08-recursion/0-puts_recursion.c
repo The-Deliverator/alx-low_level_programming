6fb051344f0941d9e9819760d6cbb080658f2ed5
The-deliverator <mdavoo@gmail.com>
2023-11-07 11:55:36 -0800

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,15 +6,11 @@
 
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
+if (*s != '\0')
 {
-	putchar('\n');
+	putchar (*s);
+	_puts_recursion(s + 1);
 }
-putchar(*s);
-_puts_recursion(s + 1);
-}
-int main(void)
-{char str[] = "recursion";
-	_puts_recursion(str);
-	return (0);
+else
+putchar ('\n');
 }
