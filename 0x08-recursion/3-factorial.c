4522634732d085a1f6c8705fe3abd8be6f112b45
The-deliverator <mdavoo@gmail.com>
2023-11-19 08:57:14 -0800

    3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,14 +6,13 @@
  *Return: a positive factorial of n
  */
 
-int factorial(int n);
+int factorial(int n)
 {
 	if (n < 0)
-		return (-1);
-else if (n == 0)
+	return (-1);
+	if (n == 0)
 	return (1);
-else
 	return (n * factorial(n - 1));
-
 }
+
 
