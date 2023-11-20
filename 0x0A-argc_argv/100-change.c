3562ff61de80b28e73a345c17eb6cf0cad4fbf9f
The-deliverator <mdavoo@gmail.com>
2023-11-19 17:30:22 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -10,9 +10,9 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents, coins = 0;
+	int cents, mncoin = 0;
 
-	if (argc != 2)
+	if (argc == 1 || argc > 2)
 	{
 		printf("Error\n");
 		return (1);
@@ -22,27 +22,18 @@ int main(int argc, char *argv[])
 
 	while (cents > 0)
 	{
-		coins++;
-		if ((cents - 25) >= 0)
-		{
+		if (cents >= 25)
 			cents -= 25;
-			continue;
-		}
-		if ((cents - 5) >= 0)
-		{
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
 			cents -= 5;
-			continue;
-		}
-		if ((cents - 2) >= 0)
-		{
+		else if (cents >= 2)
 			cents -= 2;
-			continue;
-		}
-		cents--;
-
+		else if (cents >= 1)
+			cents -= 1;
+		mncoin += 1;
 	}
-
-	printf("%d\n", coins);
-
+	printf("%d\n", mncoin);
 	return (0);
 }
