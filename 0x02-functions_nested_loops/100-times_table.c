a8870bcd03830c83f51eb0a66485ffd2a4d6391b
The-Deliverator <mdavoo@gmail.com>
2024-07-06 09:54:37 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,7 +13,7 @@ void print_times_table(int n)
 	int num, mult, prod;
 
 	if (n >= 0 && n <= 15)
-	{       
+	{
 	for (num = 0; num <= n; num++)
 	{
 	_putchar('0');
@@ -23,6 +23,8 @@ void print_times_table(int n)
 	_putchar(',');
 	prod = num * mult;
 	if (prod <= 99)
+	_putchar(' ');
+	if (prod <= 9)
 	_putchar(' ');
 	if (prod >= 100)
 	{
