25f094b18aaa9b721a7583af7c0fbefee780d9e7
The-Deliverator <mdavoo@gmail.com>
2024-07-06 13:50:10 -0700

    1-swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -3,7 +3,7 @@
 /**
  * swap_int - swaps the values of two integers.
  * @a: the first integer to be swapped.
- * @b: the second integer to be swapped 
+ * @b: the second integer to be swapped
  * Return: 0
  */
 
