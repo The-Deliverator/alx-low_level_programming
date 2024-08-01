63cfb28a417749558cba5976dd7f7338fa90a5f0
The-Deliverator <mdavoo@gmail.com>
2024-08-01 11:49:34 -0700

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,6 +7,7 @@
  */
 
 size_t print_list(const list_t *h)
+
 {
 	size_t nodes;
 
@@ -19,7 +20,7 @@ size_t print_list(const list_t *h)
 		printf("[%d] %s\n", h->len, h->str);
 	h = h->next;
 	nodes++;
-	}
+}
 return (nodes);
 }
 
